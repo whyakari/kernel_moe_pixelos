42d799a3c98b957244d05005ba97071ee76c96d1
明 <akariondev@gmail.com>
2023-12-25 23:58:51 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,69 +1,105 @@
 #!/bin/bash
 #
-# Compile script for QuicksilveR kernel
-# Copyright (C) 2020-2023 Adithya R.
+# Compile script for MoeKernel🐇
+# Copyright (C) 2020-2021 Adithya R.
 
-SECONDS=0 # builtin bash timer
-ZIPNAME="Moe-$(date '+%Y%m%d-%H%M').zip"
-TC_DIR="$HOME/tc/clang-r450784d"
-GCC_64_DIR="$HOME/tc/aarch64-linux-android-4.9"
-GCC_32_DIR="$HOME/tc/arm-linux-androideabi-4.9"
-AK3_DIR="$HOME/AnyKernel3"
-DEFCONFIG="vendor/ginkgo-perf_defconfig"
-
-MAKE_PARAMS="O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm \
-	OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip \
-	CLANG_TRIPLE=aarch64-linux-gnu- \
-	CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- \
-	CROSS_COMPILE_ARM32=$GCC_32_DIR/bin/arm-linux-androideabi-"
+SECONDS=0
+ZIPNAME="MoeKSU-$(date '+%Y%m%d').zip"
+TC_DIR="$HOME/tc/clang-18.0.0"
+GCC_64_DIR="$HOME/tc/aarch64-linux-android-14.0"
+GCC_32_DIR="$HOME/tc/arm-linux-androideabi-14.0"
+AK3_DIR="$HOME/android/AnyKernel3"
+DEFCONFIG="vendor/moe_defconfig"
 
 export PATH="$TC_DIR/bin:$PATH"
 
-if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
-   head=$(git rev-parse --verify HEAD 2>/dev/null); then
-	ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
+export KBUILD_BUILD_USER=Moe
+export KBUILD_BUILD_HOST=Nyan
+
+if ! [ -d "${TC_DIR}" ]; then
+    echo "Clang not found! Cloning to ${TC_DIR}..."
+    if ! git clone --depth=1 https://gitlab.com/moehacker/clang-r498229b ${TC_DIR}; then
+        echo "Cloning failed! Aborting..."
+        exit 1
+    fi
+fi
+
+if ! [ -d "${GCC_64_DIR}" ]; then
+    echo "gcc not found! Cloning to ${GCC_64_DIR}..."
+    if ! git clone --depth=1 -b 14 https://github.com/ZyCromerZ/aarch64-zyc-linux-gnu ${GCC_64_DIR}; then
+        echo "Cloning failed! Aborting..."
+        exit 1
+    fi
+fi
+
+if ! [ -d "${GCC_32_DIR}" ]; then
+    echo "gcc_32 not found! Cloning to ${GCC_32_DIR}..."
+    if ! git clone --depth=1 -b 14 https://github.com/ZyCromerZ/arm-zyc-linux-gnueabi ${GCC_32_DIR}; then
+        echo "Cloning failed! Aborting..."
+        exit 1
+    fi
 fi
 
 if [[ $1 = "-r" || $1 = "--regen" ]]; then
-   make $MAKE_PARAMS $DEFCONFIG savedefconfig
-   cp out/defconfig arch/arm64/configs/$DEFCONFIG
-   echo -e "\nSuccessfully regenerated defconfig at arch/arm64/configs/$DEFCONFIG"
-   exit
+    make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+    cp out/defconfig arch/arm64/configs/$DEFCONFIG
+    exit
 fi
 
 if [[ $1 = "-c" || $1 = "--clean" ]]; then
-   rm -rf out
-   echo "Cleaned output folder"
+    rm -rf out
 fi
 
-mkdir -p out
-make $MAKE_PARAMS $DEFCONFIG
-
-echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) $MAKE_PARAMS Image.gz-dtb dtbo.img 2> >(tee error.log >&2) || exit $?
-
-kernel="out/arch/arm64/boot/Image.gz-dtb"
-dtbo="out/arch/arm64/boot/dtbo.img"
-
-if [ ! -f "$kernel" ] || [ ! -f "$dtbo" ]; then
-	echo -e "\nCompilation failed!"
-	exit 1
+if [[ $1 = "-m" || $1 = "--menu" ]]; then
+    mkdir -p out
+    make O=out ARCH=arm64 $DEFCONFIG menuconfig
+elif [[ $1 = "menu" ]]; then
+    mkdir -p out
+    make O=out ARCH=arm64 $DEFCONFIG menuconfig
+else
+    mkdir -p out
+    make O=out ARCH=arm64 $DEFCONFIG
 fi
 
-echo -e "\nKernel compiled succesfully! Zipping up...\n"
-if [ -d "$AK3_DIR" ]; then
-	cp -r $AK3_DIR AnyKernel3
-	git -C AnyKernel3 checkout master &> /dev/null
-elif ! git clone -q https://github.com/ghostrider-reborn/AnyKernel3 -b master; then
-	echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
-	exit 1
+echo -e "\nStarting compilation... wait\n"
+make -j$(nproc --all) \
+    O=out \
+    ARCH=arm64 \
+    CC=clang \
+    LD=ld.lld \
+    AR=llvm-ar \
+    AS=llvm-as \
+    NM=llvm-nm \
+    OBJCOPY=llvm-objcopy \
+    OBJDUMP=llvm-objdump \
+    STRIP=llvm-strip \
+    CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- \
+    CROSS_COMPILE_ARM32=$GCC_32_DIR/bin/arm-linux-androideabi- \
+    CLANG_TRIPLE=aarch64-linux-gnu- \
+    Image.gz-dtb dtbo.img
+
+if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && \
+   [ -f "out/arch/arm64/boot/dtbo.img" ]; then
+    echo -e "\nKernel compiled successfully! Zipping up...\n"
+    if [ -d "$AK3_DIR" ]; then
+        cp -r $AK3_DIR AnyKernel3
+    elif ! git clone -q https://github.com/whyakari/AnyKernel3; then
+        echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
+        exit 1
+    fi
+    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
+    cp out/arch/arm64/boot/dtbo.img AnyKernel3
+    rm -f *zip
+    cd AnyKernel3
+    git checkout master &> /dev/null
+    zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
+    cd ..
+    rm -rf AnyKernel3
+    rm -rf out/arch/arm64/boot
+    curl --upload-file $ZIPNAME https://temp.sh/$ZIPNAME; echo
+    echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+    echo "Zip: $ZIPNAME"
+else
+    echo -e "\nCompilation failed!"
+    exit 1
 fi
-cp $kernel $dtbo AnyKernel3
-rm -rf out/arch/arm64/boot
-cd AnyKernel3
-zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
-cd ..
-rm -rf AnyKernel3
-curl --upload-file $ZIPNAME https://temp.sh/$ZIPNAME; echo
-echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-echo "$(realpath $ZIPNAME)"
